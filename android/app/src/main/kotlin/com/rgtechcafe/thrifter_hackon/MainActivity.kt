d55304d9140c82c8f58b20de9c600c976a957730
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-05-18 19:34:04 +0530

    init

---
+++ android/app/src/main/kotlin/com/rgtechcafe/thrifter_hackon/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rgtechcafe.thrifter_hackon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:thrifter_hackon/Screens/home_Screen.dart';
+import 'package:thrifter_hackon/constants.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: homeScreen,
+      routes: {
+        homeScreen: (_) => HomeScreen(),
+      },
+    );
+  }
+}
